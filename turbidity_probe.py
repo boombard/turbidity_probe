2430bc55660870aecf982079049ca6557a219376
boombard <emerton222@gmail.com>
2016-10-19 17:42:09 +0100

    first commit

---
+++ turbidity_probe.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+
+    # Generate whatever data you need in this view and send it to your template
+
+    data = [1, 2, 3]
+    return render_template("turbidity_probe_graph.html", data=data)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
